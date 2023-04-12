928dc8c58f08eefc0adbe510f797b1c1304ef073
kirthi2005 <kiruthikard@gmail.com>
2023-04-12 10:14:42 -0500

    For Loop on String

---
+++ stringForLoop.js
@@ -0,0 +1,11 @@
+//for loop over array
+var greeting = "Howdy";
+
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[1]);
+
+for(var i=0; i<greeting.length;i++){
+    console.log(greeting[i])
+}
