314f0a5b3f02e9033837f02654316dd219b2d982
kirthi2005 <kiruthikard@gmail.com>
2023-04-12 10:01:30 -0500

    string concat

---
+++ string.js
@@ -0,0 +1,9 @@
+//string is not arrays
+
+var greet = 'Hello';
+var user = 'Lisa';
+
+//console.log(greet.pop());  //TypeError: greet.pop is not a function
+
+console.log(greet + user);
+console.log(greet.concat(user));
